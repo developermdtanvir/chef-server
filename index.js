f82db14a60b178fdcd120b5ca29f2d0818a4c6f1
Tanvir <tanvir@Linux.Linux>
2023-05-04 18:49:42 +0300

    find oparetion is ready

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 
 
@@ -14,11 +16,37 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.338egrb.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+client.connect((err) => {
+    const collection = client.db(process.env.DB_USER).collection("products");
+
+    // perform actions on the collection object
+    const product = { name: "Modhu", price: 120, quantity: 30 };
+    collection.insertOne(product).then((result) => {
+        console.log("One product added");
+    });
+    console.log("database connected");
+});
+
+
+const chefCollection = client.db('chef').collection('all_chef');
+
+
 async function run() {
     try {
 
-        app.get('/chef', (req, res) => {
-
+        app.get('/chef', async (req, res) => {
+            const query = {};
+            const cursor = await chefCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
         })
 
 
@@ -33,4 +61,4 @@ app.get('/', (req, res) => {
     res.send('server is running')
 })
 
-app.listen(2000, () => console.log("hello world"))
+app.listen(3000, () => console.log("hello world"))
