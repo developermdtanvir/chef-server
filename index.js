b3ee4abdff6e6f2a8acb64ba0660f4fff53646a8
Tanvir <tanvir@Linux.Linux>
2023-05-05 19:23:04 +0300

    cors problem fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // middleware 
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.338egrb.mongodb.net/?retryWrites=true&w=majority`;
 
